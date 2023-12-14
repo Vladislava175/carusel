bcebf612691db4dc509207eea0abfe71b00bad9b
vladislava zhyguliov <vladislava.zhyguliovtsev@allcloud.io>
2023-12-14 17:26:27 +0200

    init code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CaruselComponent } from "./carusel/carusel.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CaruselComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/carusel/carusel.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from "@angular/core";
+import { ISlide } from "../models/slide";
+
+@Component({
+  selector: "app-carusel",
+  template: `
+    <div class="slider">
+      <div>
+        <div class="arrow left" (click)="leftArrowClick()">❰</div>
+        <div class="arrow right" (click)="rightArrowClick()">❱</div>
+      </div>
+      <div
+        class="slide"
+        [ngStyle]="{ 'background-image': currentSlideUrl }"
+      ></div>
+      <h1 class="title">
+        {{ slideList[currentIndex].title }}
+      </h1>
+      <div class="dots">
+        <button
+          (click)="currentIndex = i"
+          *ngFor="let slide of slideList; let i = index"
+          [class]="'dot ' + (i === currentIndex ? 'active' : '')"
+        ></button>
+      </div>
+    </div>
+  `,
+  styleUrls: ["carusel.component.css"],
+})
+export class CaruselComponent {
+  @Input() slideList: ISlide[] = [];
+  currentIndex: number = 0;
+
+  get currentSlideUrl() {
+    return `url('${this.slideList[this.currentIndex].url}')`;
+  }
+  leftArrowClick() {
+    this.currentIndex !== 0 && this.currentIndex--;
+  }
+  rightArrowClick() {
+    this.currentIndex < this.slideList.length && this.currentIndex++;
+  }
+}
